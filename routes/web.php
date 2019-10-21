3d93c37dd024c41826a62c7e321e5e6ea318a4b4
PengHuieSin <eason12326@gmail.com>
2019-10-21 18:03:05 +0800

    練習2-1

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,20 @@
 */
 
 //練習1：設定 Route 反應
-Route::get('/', function () {
-    // return view('welcome');
-    //練習 1-1
-    // return 'welcome';
-    //練習 1-2
-    //return view('welcome');
-    //練習 1-3
-    return redirect('welcome');
+//Route::get('/', function () {
+// return view('welcome');
+//練習 1-1
+// return 'welcome';
+//練習 1-2
+//return view('welcome');
+//練習 1-3
+//return redirect('welcome');
+//});
+
+//練習2：設定 Route 接收參數
+//練習2-1
+Route::get('hello/{name}',function($name){
+    return'Hello,'.$name;
 });
+
 
