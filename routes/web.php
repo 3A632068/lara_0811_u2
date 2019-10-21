2e55a464d514eed3591e095f13b81319541ed180
PengHuieSin <eason12326@gmail.com>
2019-10-21 17:45:38 +0800

    練習 1：設定 Route 反應

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    //練習 1-1
+    // return 'welcome';
+    //練習 1-2
+    //return view('welcome');
+    //練習 1-3
+    return redirect('welcome');
 });
